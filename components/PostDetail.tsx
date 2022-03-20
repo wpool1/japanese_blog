460ba7f31652e997c144be743e36494893b8098c
Winston Pool <winston.pool@nike.com>
2022-03-20 17:43:09 +0900

    Added any explicit typing to PostDetail.tsx.

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -21,11 +21,11 @@ export const PostDetail = ({ post }: {post: any} ) => {
     
         switch (type) {
             case 'heading-three':
-                return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+                return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item: any, i: any) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
             case 'paragraph':
-                return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+                return <p key={index} className="mb-8">{modifiedText.map((item: any, i: any) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
             case 'heading-four':
-                return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+                return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item: any, i: any) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
             case 'image':
                 return (
                     <img
@@ -72,8 +72,8 @@ export const PostDetail = ({ post }: {post: any} ) => {
                     </div>
                 </div>
                 <h1 className="mb-8 text-2xl font-semibold">{post.title}</h1>
-                {post.content.raw.children.map((typeObj, index) => {
-                    const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+                {post.content.raw.children.map((typeObj: any, index: any) => {
+                    const children = typeObj.children.map((item: any, itemIndex: any) => getContentFragment(itemIndex, item.text, item))
 
                     return getContentFragment(index, children, typeObj, typeObj.type)
                 })}
