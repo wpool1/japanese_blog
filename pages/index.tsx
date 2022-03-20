8e1b0bdf7e758e1885a2a4ed92599cea83a14be6
Winston Pool <winston.pool@nike.com>
2022-03-20 18:30:32 +0900

    Added any explicit typing to Index.tsx.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services'
 
-const Home: NextPage = ({ posts } : { posts: any }) => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -13,7 +13,7 @@ const Home: NextPage = ({ posts } : { posts: any }) => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: any) => <PostCard post={post.node} key={post.title}/>)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title}/>)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
